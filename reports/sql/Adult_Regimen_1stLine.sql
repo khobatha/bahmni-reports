023808ffb53ae46c1e644a6c2ae007ce79c94756
ksetetemela <khobatha.setetemela@gmail.com>
2021-08-03 15:53:41 +0200

    Update Adult_Regimen_1stLine.sql

--- reports/sql/Adult_Regimen_1stLine.sql
+++ reports/sql/Adult_Regimen_1stLine.sql
@@ -923,6 +923,37 @@ FROM
 				  )Seen_Clients)
 
  UNION
+ 
+ union 
+select '','','1c=AZT-3TC-NVP'	
+union 
+select '','','1d=AZT-3TC-EFV'
+union 
+select '','','1e=TDF-3TC-NVP'
+union 
+select '','','1f=TDF-3TC-EFV'
+union 
+select '','','1f(a)TDF 3TC /EFV (400mg)'
+union 
+select '','','1g=ABC-3TC-NVP'
+union 
+select '','','1h=ABC-3TC-EFV'
+union 
+select '','','1j=TDF-3TC-DTG'
+union 
+select '','','1k=ABC-3TC-DTG'
+union 
+select '','','1m=AZT-3TC-DTG'
+union 
+select '','','1n=TDF-3TC-LPV/r'
+union 
+select '','','1p=ABC-3TC-LPV/r'
+union 
+select '','','1q=AZT-3TC-LPV/r'
+union 
+select '','','Other'
+ 
+ UNION
 
  (Select Id, outcome, Age
 	FROM
